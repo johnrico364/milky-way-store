50843d96b8c95f43e6cce197f4ca57c04d0f8edc
John Anthony Rico <johnrico364@gmail.com>
2025-03-29 19:37:06 +0800

    Update: Update  mongoose query

--- server/controllers/orderControllers.js
+++ server/controllers/orderControllers.js
@@ -198,7 +198,15 @@ const updateDeliveryStatus = async (req, res) => {
 
 const getTransactionHistory = async (req, res) => {
   try {
-    const orders = await Order.find({ isDelivered: true });
+    const orders = await Order.find({ isDelivered: true })
+      .populate({
+        path: "product",
+        select: "name price",
+      })
+      .populate({
+        path: "ordered_by",
+        select: "fname lname",
+      });
     res.status(200).json({ orders });
   } catch (error) {
     res.status(400).json({ error: error.message });
