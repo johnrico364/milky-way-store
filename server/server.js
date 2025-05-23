754b3fa045aa42d49fcbff73d375a380c0063248
John Anthony Rico <johnrico364@gmail.com>
2025-05-23 22:55:30 +0800

    try

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors')
+const cors = require("cors");
 require("dotenv").config();
 
 //routes
@@ -9,13 +9,7 @@ const productRoutes = require("./routes/product");
 const orderRoutes = require("./routes/order");
 
 const app = express();
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
@@ -30,3 +24,12 @@ app.listen(3001, () => console.log("Listening to post 3001"));
 app.use("/api/user", userRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/order", orderRoutes);
+
+
+/*
+{
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  } 
+ */
