6d93dbd890e4abbe737f0e6962992542193d4525
John Anthony Rico <johnrico364@gmail.com>
2025-05-25 15:29:57 +0800

    Remove vercel config

--- server/server.js
+++ server/server.js
@@ -12,8 +12,6 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    exposedHeaders: ['Access-Control-Allow-Origin'],
     credentials: true,
   })
 );
