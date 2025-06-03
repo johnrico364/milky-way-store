1356ef817ef8dfbe28f1f85d2a03b47312e7c510
John Anthony Rico <johnrico364@gmail.com>
2025-06-04 00:57:45 +0800

    Fix auth issue

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,8 +1,38 @@
+import { useEffect } from "react";
 import { Screen1 } from "../components/landingScreens/Screen1";
 import { Screen2 } from "../components/landingScreens/Screen2";
 import { Screen3 } from "../components/landingScreens/Screen3";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const LandingPage = () => {
+  const navigate = useNavigate();
+
+  const user = JSON.parse(localStorage.getItem("user") || `{"token": "null"}`);
+  const axiosInstance = axios.create({
+    headers: {
+      Authorization: `Bearer ${user.token}`,
+    },
+  });
+
+  const checkAuthUser = async () => {
+    try {
+      const user = await axiosInstance.get(
+        `${process.env.REACT_APP_BACKEND_BASEURL}/api/user/auth-token`
+      );
+
+      if (user.data.mess) {
+        navigate(user.data.isAdmin ? "/admin/dashboard" : "/user/products");
+      }
+    } catch (error: any) {
+      console.log(error.response.data);
+    }
+  };
+
+  useEffect(() => {
+    checkAuthUser();
+  }, []);
+
   return (
     <div>
       <Screen1 />
